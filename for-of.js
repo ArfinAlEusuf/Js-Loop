37a05d360bdd0a151db81bbe71a5aaa479687578
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-27 21:40:12 +0600

    for of

---
+++ for-of.js
@@ -0,0 +1,13 @@
+const numbers = [12, 98, 45, 63 , 21, 72];
+
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+}
+
+const fruits = ['orange', 'mango', 'Apple', 'painapple'];
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('i want to eat');
+}
