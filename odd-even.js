431aee6d9f73ed2a67c12947d441f0480c786691
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-27 23:55:49 +0600

    odd and even by for loop

---
+++ odd-even.js
@@ -0,0 +1,14 @@
+// for (let i=0; i<10; i++){
+//     console.log(i);
+// }
+
+
+// odd number 
+for (let i= 1; i <= 50; i=i+2){
+    console.log(i);
+}
+
+// even number 
+for (let i= 0; i <= 50; i=i+2){
+    console.log(i);
+}
